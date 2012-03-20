47924302ff8af05cafcd122753ebb24d4f489162
dragon.thavisha@gmail.com <dragon.thavisha@gmail.com@c9ead0c7-6394-5fdd-6956-1982a9dc6c1c>
2012-03-20 19:57:11 +0000

    * Add new Config option (SERVER_LISTEN_PORT) to specify which port the server should listen for connections on (default: 7777) * Convert KatanaServer into a Singleton Object. Considering doing the same to PingServer

--- trunk/src/server/ServerMain.java
+++ trunk/src/server/ServerMain.java
@@ -1,10 +1,6 @@
 package server;
 
 import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 
 import shared.Constants;
 import shared.KatanaPacket;
@@ -22,23 +18,29 @@ public class ServerMain
                 Config.getConfig(Constants.CONFIG_SQL_USERNAME),
                 Config.getConfig(Constants.CONFIG_SQL_PASSWORD));
         
-        KatanaServer katana = new KatanaServer();
+        KatanaServer.initSingleton(
+                Integer.parseInt(Config.getConfig(Constants.CONFIG_SERVER_PORT)));
         PingServer ping = new PingServer(
                 Integer.parseInt(Config.getConfig(Constants.CONFIG_PING_INTERVAL)));
     }
     
-
     // Remove this method, just for testing
     public static void testStuff()
     {
         try
         {
+            // Very hacky, but required because otherwise packets are sent immediately to the server
+            // Since there are multiple threads running here, the server thread might not be ready 
+            // (since the singleton may not be set up yet), and given the way it's been coded - it crashes.
+            // This is intentional because if we somehow are unable to set up the Server's Singleton thread...
+            //and attempt to access it, we should kill everything instead of letting things like SQL or Ping run..
+            Thread.currentThread().sleep(5000);
             KatanaPacket packet = new KatanaPacket(0, Opcode.C_LOGIN);
             packet.addData("katana"); // username
             packet.addData("password"); // password
             packet.addData("321.19, 291.32"); // location
             
-            KatanaSocket.sendPacket(InetAddress.getLocalHost().getHostAddress(), Constants.SERVER_LISTEN_PORT, packet);
+            KatanaSocket.sendPacket(InetAddress.getLocalHost().getHostAddress(), KatanaServer.instance().getPort(), packet);
         }
         catch(Exception ex)
         {
