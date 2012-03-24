872cf483c194ee20ac204e151834feaeb30d7c14
szoieia@gmail.com <szoieia@gmail.com@c9ead0c7-6394-5fdd-6956-1982a9dc6c1c>
2012-03-24 03:12:37 +0000

    * Update Splash for most recent changes

--- trunk/src/client/Splash/src/com/katana/splash/SplashActivity.java
+++ trunk/src/client/Splash/src/com/katana/splash/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.katana.splash;
 
+import shared.KatanaConstants;
 import shared.KatanaPacket;
 import shared.Opcode;
 import android.app.Activity;
@@ -13,17 +14,14 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.Looper;
-import android.widget.Toast;
 
 import com.katana.splash.KatanaService.LocalBinder;
 
 public class SplashActivity extends Activity {
-	SharedPreferences pref;
-	public static final String PREFS_NAME = "UserPreferences";
-	private static final String PREF_USERNAME = "username";
-	private static final String PREF_PASSWORD = "password";
-	
-	//Service Vars
+
+	// Preferences
+	private SharedPreferences pref;
+	// KatanaService
 	KatanaService katanaService;
 	boolean mBound = false;
 	
@@ -34,27 +32,23 @@ public class SplashActivity extends Activity {
         setContentView(R.layout.splash);
         
         Thread splashTimer = new Thread(){
-        	@SuppressWarnings("static-access")
+			@SuppressWarnings("static-access")
 			public void run() {     
         		Looper.myLooper().prepare();
         		try {
         			sleep(3000);
         			
         			// Check preferences file for stored user/pass
-        			pref = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
-        			String user = pref.getString(PREF_USERNAME, null);
-        			String pass = pref.getString(PREF_PASSWORD, null);
+        			pref = getSharedPreferences(KatanaConstants.PREFS_LOGIN, MODE_PRIVATE);
+        			String user = pref.getString(KatanaConstants.LOGIN_USER, null);
+        			String pass = pref.getString(KatanaConstants.LOGIN_PASS, null);
         			
         			if (user == null || pass == null){
-        				Intent i = new Intent();
-            			i.setClass(SplashActivity.this, LoginActivity.class);
-            			startActivity(i);
-            			finish();
+        				// No user login stored
+        				startLoginActivity();
         			} else {
-        				KatanaPacket packet = new KatanaPacket(0,Opcode.C_LOGIN);
-        				packet.addData(user);
-        				packet.addData(pass);
-        				katanaService.sendPacket(packet);
+        				// User login found, send to server
+        				checkLogin(user,pass);
         			}	
         		} catch (InterruptedException e) {
         			e.printStackTrace();
@@ -63,34 +57,51 @@ public class SplashActivity extends Activity {
         };
         
         splashTimer.start();
-        Intent intent = new Intent(this,KatanaService.class);
-        startService(intent);
-        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
-        registerReceiver(broadcastReceiver, new IntentFilter(KatanaService.BROADCAST_ACTION));
+        
+        // Bind to KatanaService
+        doBindService();
+        
     }
     
-    void doUnbindService() {
+    /** Final cleanup when activity is finished */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+     // Unbind KatanaService and Receiver
+        doUnbindService();
+    }
+    
+    /** Disconnect from KatanaService */
+    private void doUnbindService() {
         if (mBound) {
-            // Detach our existing connection.
+            // Detach our existing connection and broadcast receiver
             unbindService(mConnection);
             unregisterReceiver(broadcastReceiver);
             stopService(new Intent(SplashActivity.this,KatanaService.class));
             mBound = false;
         }
     }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        doUnbindService();
+    
+    /** Verify user credentials with server */
+    private void checkLogin(String user, String pass){
+		KatanaPacket packet = new KatanaPacket(0,Opcode.C_LOGIN);
+		packet.addData(user);
+		packet.addData(pass);
+		katanaService.sendPacket(packet);
     }
     
-    private ServiceConnection mConnection = new ServiceConnection() {
+    /** Connect to KatanaService */
+    private void doBindService() {
+        Intent intent = new Intent(this,KatanaService.class);
+        startService(intent);
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+        registerReceiver(broadcastReceiver, new IntentFilter(KatanaService.BROADCAST_ACTION));
+    }
 
+    private ServiceConnection mConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName className, IBinder service) {
             // We've bound to LocalService, cast the IBinder and get LocalService instance
-        	System.out.println("Service is bound!");
             LocalBinder binder = (LocalBinder) service;
             katanaService = binder.getService();
             mBound = true;
@@ -102,20 +113,30 @@ public class SplashActivity extends Activity {
         }
     };
     
+    /** on Receive broadcast from KatanaService */
     private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
     	@Override
     	public void onReceive(Context context, Intent intent) {
-    		if(intent.getStringExtra("opCode").equals(Opcode.S_AUTH_OK.name())){
-    			Intent i = new Intent();
-    			i.setClass(SplashActivity.this, LobbyActivity.class);
-    			startActivity(i);
-    			finish();
-    		} else if(intent.getStringExtra("opCode").equals(Opcode.S_AUTH_NO.name())){
-    			Intent i = new Intent();
-    			i.setClass(SplashActivity.this, LoginActivity.class);
-    			startActivity(i);
-    			finish();
+    		if(intent.getStringExtra(KatanaService.EXTRAS_OPCODE).equals(Opcode.S_AUTH_OK.name())){
+    			startLobbyActivity();
+    		} else if(intent.getStringExtra(KatanaService.EXTRAS_OPCODE).equals(Opcode.S_AUTH_NO.name())){
+    			startLoginActivity();
     		} 
     	}
     };
+    
+    /** Switch user to appropriate activities */
+    private void startLoginActivity(){
+    	Intent i = new Intent();
+		i.setClass(SplashActivity.this, LoginActivity.class);
+		startActivity(i);
+		finish();
+    }
+    
+    private void startLobbyActivity(){
+    	Intent i = new Intent();
+		i.setClass(SplashActivity.this, LobbyActivity.class);
+		startActivity(i);
+		finish();
+    }
 }
