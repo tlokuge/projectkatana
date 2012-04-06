4111f6d8cbcfa25da37bfb6dfd5afcb9dffd1b27
dragon.thavisha@gmail.com <dragon.thavisha@gmail.com@c9ead0c7-6394-5fdd-6956-1982a9dc6c1c>
2012-04-06 18:47:15 +0000

    * Add missing KatanaError file missing in [79]

---
+++ src/server/KatanaError.java
@@ -0,0 +1,22 @@
+package server;
+
+public enum KatanaError 
+{
+    /** Error Codes **/
+    // SQL
+    ERR_SQL_CONN_LOST,
+    ERR_SQL_CONN_FAIL,
+    ERR_SQL_SINGLETON_LOST,
+
+    // Config
+    ERR_CONFIG_LOAD_FAIL,
+
+    // Server
+    ERR_SERVER_LISTEN_FAIL,
+    ERR_SERVER_SINGLETON_LOST,
+
+    // SQLCache
+    ERR_SQLCACHE_NO_LOCATIONS,
+    ERR_SQLCACHE_NO_SPELLS,
+    ERR_SQLCACHE_NO_CLASSES,
+};
