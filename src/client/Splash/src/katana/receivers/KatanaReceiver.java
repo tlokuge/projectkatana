079bc69957658f203080744c9c3f3ddd939b1dd4
szoieia@gmail.com <szoieia@gmail.com@c9ead0c7-6394-5fdd-6956-1982a9dc6c1c>
2012-04-07 18:58:55 +0000

    * LocationReceiver, KatanaReceiver Moved BroadcastReceivers to their own class
    * SplashActivity, LoginActivity, LobbyActivity
    Updated to reflect moved BroadcastReceivers

---
+++ src/client/Splash/src/katana/receivers/KatanaReceiver.java
@@ -0,0 +1,95 @@
+package katana.receivers;
+
+import java.util.ArrayList;
+
+import katana.services.KatanaService;
+import katana.shared.Opcode;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+import com.katana.splash.LobbyActivity;
+import com.katana.splash.LoginActivity;
+
+public class KatanaReceiver extends BroadcastReceiver {
+	private int mode;
+	
+	public KatanaReceiver(int m){
+		mode = m;
+	}
+	
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		if(mode == 0) {
+			// SplashActivity
+    		if(intent.getStringExtra(KatanaService.EXTRAS_OPCODE).equals(Opcode.S_AUTH_OK.name())){
+    			startMyActivity(context, LobbyActivity.class);
+    		} else if(intent.getStringExtra(KatanaService.EXTRAS_OPCODE).equals(Opcode.S_AUTH_NO.name())){
+    			startMyActivity(context, LoginActivity.class);
+    		} 
+		} if(mode == 1) {
+			// LoginActivity
+			LoginActivity loginActivity = (LoginActivity) context;
+			if(intent.getStringExtra(KatanaService.EXTRAS_OPCODE).equals(Opcode.S_REG_OK.name())){
+    			// Registered new user on server    			
+    			loginActivity.setUserLoginPrefs(true);
+    		} else if(intent.getStringExtra(KatanaService.EXTRAS_OPCODE).equals(Opcode.S_AUTH_OK.name())){
+    			loginActivity.setUserLoginPrefs(false);
+    		} else if(intent.getStringExtra(KatanaService.EXTRAS_OPCODE).equals(Opcode.S_REG_NO.name())){
+    			Toast.makeText(loginActivity, "Username already exists or wrong password!", Toast.LENGTH_SHORT).show();
+    		} 
+		} if(mode == 2) {
+			LobbyActivity lobby = (LobbyActivity) context;
+			if(intent.getStringExtra(KatanaService.EXTRAS_OPCODE).equals(Opcode.S_ROOM_LIST.name())){
+				lobby.inValidLoc = true;
+    			String locName = intent.getStringExtra(KatanaService.EXTRAS_LOCNAME);
+    			ArrayList<String> al = intent.getStringArrayListExtra(KatanaService.EXTRAS_ROOMSLIST);
+    			lobby.showRoomList(locName, al);
+    		} else if (intent.getStringExtra(KatanaService.EXTRAS_OPCODE).equals(Opcode.S_ROOM_JOIN_OK.name())) {
+    			ArrayList<String> al = intent.getStringArrayListExtra(KatanaService.EXTRAS_PLAYERLIST);
+    			lobby.showRoomPlayers(al);
+    			lobby.joinRoom(lobby.selectedRoom);
+    		} else if (intent.getStringExtra(KatanaService.EXTRAS_OPCODE).equals(Opcode.S_ROOM_JOIN_NO.name())) {
+    			Toast.makeText(lobby, "You cant join this room.", Toast.LENGTH_SHORT).show();
+    		} else if (intent.getStringExtra(KatanaService.EXTRAS_OPCODE).equals(Opcode.S_ROOM_PLAYER_JOIN.name())) {
+    			lobby.addPlayer(intent);
+    			
+    		} else if (intent.getStringExtra(KatanaService.EXTRAS_OPCODE).equals(Opcode.S_ROOM_PLAYER_LEAVE.name())) {
+    			lobby.removePlayer(Integer.parseInt((intent.getStringExtra(KatanaService.EXTRAS_PLAYERNAME))));
+    		}
+    		else if(intent.getStringExtra(KatanaService.EXTRAS_OPCODE).equals(Opcode.S_ROOM_CREATE_OK.name()))
+    		{
+    			lobby.setCreatedRoomId(Integer.parseInt(intent.getStringExtra(KatanaService.EXTRAS_ROOMID)));
+    			lobby.joinCreatedRoom();
+    			lobby.refreshList(null);
+    		}
+    		else if(intent.getStringExtra(KatanaService.EXTRAS_OPCODE).equals(Opcode.S_ROOM_CREATE_NO.name()))
+    		{
+    			lobby.setSelectedRoom(null);
+    		} else if(intent.getStringExtra(KatanaService.EXTRAS_OPCODE).equals(Opcode.S_PLAYER_UPDATE_CLASS.name())) {
+    			if(lobby.inRoom){
+    				int playerid = intent.getIntExtra(KatanaService.EXTRAS_PLAYERID, -1);
+    				int playerclass = intent.getIntExtra(KatanaService.EXTRAS_PLAYERCLASS, -1);
+    				lobby.updatePlayerClass(playerid, playerclass);
+    			}
+    		} else if(intent.getStringExtra(KatanaService.EXTRAS_OPCODE).equals(Opcode.S_ROOM_DESTROY.name())) {
+    			if(lobby.inRoom){
+    				lobby.refreshList(null);
+    				lobby.setInRoom(false);
+    				lobby.setRoomLeader(false);
+    				lobby.transitionToLobby();
+    			}
+    		} else if(intent.getStringExtra(KatanaService.EXTRAS_OPCODE).equals(Opcode.S_LEADERBOARD.name())) {
+    			lobby.showLeaderboardDialog(intent.getStringExtra(KatanaService.EXTRAS_SCORES));
+    		}
+		}
+	}
+	
+	@SuppressWarnings("rawtypes")
+	public void startMyActivity(Context context, Class myClass) {
+		Intent intent = new Intent();
+		intent.setClass(context, myClass);
+		context.startActivity(intent);
+	}
+}
