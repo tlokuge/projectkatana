a8fc1554575e93f71d9c7ed1f5f520fba68a97cd
dragon.thavisha@gmail.com <dragon.thavisha@gmail.com@c9ead0c7-6394-5fdd-6956-1982a9dc6c1c>
2012-04-10 00:56:32 +0000

    * Implement map class. * NOTE: Player and Creature updates will now be handled through maps rather than globally.

---
+++ src/server/Map.java
@@ -0,0 +1,59 @@
+package server;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Map
+{
+    private int id;
+    private int guid;
+    private int location_id;
+    
+    private String name;
+    private String background;
+    
+    private ArrayList<Integer> player_list;
+    private HashMap<Integer, Creature> creature_map;
+    
+    private static int NEXT_MAP_GUID = 0;
+    
+    public Map(int id)
+    {
+        MapTemplate template = SQLCache.getMap(id);
+        
+        this.id          = template.getId();
+        this.guid        = getNextMapGUID();
+        this.location_id = template.getLocation();
+        this.name        = template.getName();
+        this.background  = template.getBackground();
+        
+        this.player_list  = new ArrayList<>();
+        this.creature_map = new HashMap<>();
+    }
+    
+    public static int getNextMapGUID() { return NEXT_MAP_GUID++; }
+    
+    public int getMapId()           { return id; }
+    public int getGUID()            { return guid; }
+    public int getLocationId()      { return location_id; }
+    public String getName()         { return name; }
+    public String getBackground()   { return background; }
+    
+    public void addPlayer(Player pl) { player_list.add(pl.getId()); }
+    
+    public Creature spawnCreature(int id)
+    {
+        Creature creature = new Creature(id);
+        creature_map.put(creature.getGUID(), creature);
+        return creature;
+    }
+    
+    public void update(int diff)
+    {
+        for(int id : player_list)
+            KatanaServer.instance().getPlayer(id).update(diff);
+        
+        for(int id : creature_map.keySet())
+            creature_map.get(id).update(diff);
+    }
+}
