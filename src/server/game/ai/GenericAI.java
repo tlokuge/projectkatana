f617f855faa583be48b4cf9bda760dcb41d00dca
dragon.thavisha@gmail.com <dragon.thavisha@gmail.com@c9ead0c7-6394-5fdd-6956-1982a9dc6c1c>
2012-04-17 13:09:17 +0000

    * GenericAI now moves and finds watermelons. * Game now lasts 1 minute (up from 30 seconds) * Fix some crashes in Map

--- src/server/game/ai/GenericAI.java
+++ src/server/game/ai/GenericAI.java
@@ -1,14 +1,10 @@
-package server.game.ai;
+ package server.game.ai;
 
 import java.util.ArrayList;
-import server.game.Creature;
-import server.game.CreatureAI;
-import server.game.Map;
-import server.game.Player;
+import java.util.Iterator;
+import server.game.*;
 import server.handlers.GameHandler;
 import server.shared.Constants;
-import server.shared.KatanaPacket;
-import server.shared.Opcode;
 
 public class GenericAI extends CreatureAI
 {
@@ -16,7 +12,7 @@ public class GenericAI extends CreatureAI
     
     private int spawnTimer;
     private int spawnInterval;
-    private int gameTimer;
+    private int checkTimer;
     
     public ArrayList<Integer> creature_spawns;
     
@@ -34,7 +30,7 @@ public class GenericAI extends CreatureAI
         
         moveTimer = 5000;
         spawnTimer = 100;
-        gameTimer = Constants.GAME_END_TIMER;
+        checkTimer = 500;
         
         Map map = GameHandler.instance().getMap(m_creature.getMap());
         if(map == null)
@@ -56,45 +52,138 @@ public class GenericAI extends CreatureAI
         if(move == null)
             return;
         
-        float move_x = move.getX();
-        float move_y = move.getY();
-        
         //System.out.println("CHECKING MOVEMENT FOR PLAYER " + move + " - " + move_x + " , " + move_y);
-        ArrayList<Integer> removeList = new ArrayList<Integer>();
-        for(Integer cguid : creature_spawns)
+        Iterator<Integer> itr = creature_spawns.iterator();
+        while(itr.hasNext())
         {
+            int cguid = itr.next();
             Creature spawn = getCreatureFromMap(cguid);
             if(spawn == null)
             {
-                removeList.add(cguid);
+                itr.remove();
                 continue;
             }
             
-            float cx = spawn.getX();
-            float cy = spawn.getY();
-//            System.out.println("===========");
-//            System.out.println(cx + " - " + move_x + " = " + Math.abs(cx - move_x));
-//            System.out.println(cy + " - " + move_y + " = " + Math.abs(cy - move_y));
-            
-            if(Math.abs(cx - move_x) < MIN_DISTANCE && Math.abs(cy - move_y) < MIN_DISTANCE)
+            if(isWithinRangeOf(move, spawn, MIN_DISTANCE))
             {
                 move.addPoints(1);
                 System.out.println("PLAYER " + move + " : " + move.getPoints());
-                creature_spawns.remove(cguid);
+                System.out.println("Removing " + cguid);
+                itr.remove();
                 despawnCreature(cguid);
-                return;
+                break;
             }
         }
+    }
+    
+    private boolean isWithinRangeOf(Unit unit, Unit target, float range)
+    {
+        if(unit == null || target == null)
+            return false;
+        
+        float ux = unit.getX();
+        float uy = unit.getY();
+        float tx = target.getX();
+        float ty = target.getY();
+        
+        if(Math.abs(ux - tx) < range && Math.abs(uy - ty) < range)
+            return true;
+        
+        return false;
+    }
+    
+    private int calculateTravelTime(float start_x, float start_y, float dest_x, float dest_y)
+    {
+        float dx = Math.abs(start_x - dest_x);
+        float dy = Math.abs(start_y - dest_y);
+        float hyp = (float)Math.sqrt((dx * dx) + (dy * dy));
+        int time =(int) hyp * 5;
+        System.out.println("[" + start_x + "," + start_y + "] -> [" + dest_x + "," + dest_y + "] = " + time);
+        return time;
+    }
+    
+    private int moveToTarget(Unit target)
+    {
+        if(target == null)
+            return -1;
+        
+        System.out.println("Mastoras moving to " + target);
+        float mx = m_creature.getX();
+        float my = m_creature.getY();
+        float dest_x = target.getX();
+        float dest_y = target.getY();
+        m_creature.moveTo(dest_x, dest_y);
+        return calculateTravelTime(mx, my, dest_x, dest_y);
+    }
+    
+    private int moveToRandomLocation()
+    {
+        System.out.println("Mastoras random move");
+        Map map = GameHandler.instance().getMap(m_creature.getMap());
+        if(map == null)
+        {
+            System.err.println("omg map nul moveToRandmLoc");
+            return -1;
+        }
+        
+        float mx = m_creature.getX();
+        float my = m_creature.getY();
+        float dest_x = map.getRandX();
+        float dest_y = map.getRandY();
+        m_creature.moveTo(dest_x, dest_y);
+        
+        return calculateTravelTime(mx, my, dest_x, dest_y);
     }
     
     public void updateAI(int diff)
     {        
         if(moveTimer < diff)
         {
-            Map map = GameHandler.instance().getMap(m_creature.getMap());
-            m_creature.moveTo(map.getRandX(), map.getRandY());
-            moveTimer = 500 + GameHandler.instance().getRandInt(1000);
+            if(!creature_spawns.isEmpty())
+            {
+                Map m = GameHandler.instance().getMap(m_creature.getMap());
+                int tar_guid = creature_spawns.get(GameHandler.instance().getRandInt(creature_spawns.size()));
+                if(m != null)
+                    moveTimer = moveToTarget(m.getCreature(tar_guid));
+                else
+                    moveTimer = moveToRandomLocation();
+            }
+            else
+                moveTimer = moveToRandomLocation();
+            if(moveTimer < 0)
+                moveTimer = moveToRandomLocation();
+            
+            moveTimer += 200;
+            checkTimer = moveTimer - 100;
+            
+            System.out.println("moveTimer: " + moveTimer + " checkTimer: " + checkTimer);
         }else moveTimer -= diff;
+        
+        if(checkTimer < diff)
+        {
+            System.out.println("Checking...");
+            Map m = GameHandler.instance().getMap(m_creature.getMap());
+            if(m != null)
+            {
+                Iterator<Integer> itr = creature_spawns.iterator();
+                while(itr.hasNext())
+                {
+                    int cguid = itr.next();
+                    if(isWithinRangeOf(m.getCreature(cguid), m_creature, MIN_DISTANCE))
+                    {
+                        itr.remove();
+                        despawnCreature(cguid);
+                        System.err.println("Mastoras eating " + cguid);
+                        break;
+                    }
+                }
+            }
+            System.out.println(creature_spawns);
+            System.out.println(m.getCreatures().keySet());
+            
+            moveTimer = 500;
+            checkTimer = 500;
+        }else checkTimer -= diff;
         
         if(creature_spawns.size() >= 10)
             return;
