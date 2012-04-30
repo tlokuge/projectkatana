56ae116bc2a43bae86b9e521dbf9c5068c6a6f77
dragon.thavisha@gmail.com <dragon.thavisha@gmail.com@c9ead0c7-6394-5fdd-6956-1982a9dc6c1c>
2012-04-30 17:59:05 +0000

    * First wave of javadoc-style documentation on server: KatanaClient, KatanaServer, Creature, CreatureAI * Remove getNextGUID within Creature and Map, just use the GameHandler's one. * Call appropriate AI functions for spells from creature (although they are still unused)

--- src/server/game/Creature.java
+++ src/server/game/Creature.java
@@ -4,24 +4,36 @@ import server.handlers.GameHandler;
 import server.templates.CreatureTemplate;
 import server.utils.SQLCache;
 
+/**
+ * Every non player character (NPC) is a creature (which is a child of unit), containing some base stats (which are currently not being used) and its own AI
+ */
 public class Creature extends Unit
 {
-    private int entry;
-    private int level;
+    private int entry; // Creature ID in the creatures datanase table
+    private int level; // Creature level (UNUSED)
     
-    private CreatureAI ai;
+    private CreatureAI ai; // The AI instance for this creature
     
+    /**
+     * Instantiates a new Creature object
+     * @param entry The ID of the creature from the creatures database table
+     * @param map_id The map that the creature is in
+     */
     public Creature(int entry, int map_id)
     {
-        super(getNextGUID(), "", map_id, 0, 0, 0, 0f, 0);
+        // Calls the Unit constructor (the creature's ID is a GUID taken from the GameHandler). 
+        // Every other value passed (besides the map_id) is default to 0 and will be set via the CreatureTemplate below.
+        super(GameHandler.instance().getNextGUID(), "", map_id, 0, 0, 0, 0f, 0);
         
         this.entry = entry;
+        // Load the template for the creature entry (Cached on server startup)
         CreatureTemplate template = SQLCache.getCreature(entry);
         if(template == null)
         {
-            System.err.println("OMG ATTEMPTED TO SPAWN INVALID CREATURE (entry: " + entry + ")");
-            return;
+            System.err.println("Attempted to spawn invalid creature (entry: " + entry + ")");
+            return; // What should we do if this happens? Spawn a default?
         }
+        // Set all stats
         this.setName(template.getCreatureName());
         this.setMaxhealth(template.getHealth());
         this.setLevel(template.getLevel());
@@ -30,46 +42,102 @@ public class Creature extends Unit
         this.setSpeed(template.getMoveSpeed());
         this.setModelId(template.getModelId());
         this.addToMap(map_id);
+        // Load the AI by the AI name, and pass this to the AI so it has access to the actual creature
         this.ai = CreatureAI.getAIByName(template.getScript(), Creature.this);
     }
     
-    public static int getNextGUID() { return GameHandler.instance().getNextGUID(); }
-    
-    public int getEntry() { return entry; }
-    
+    // Simple accessors and mutators
+    public int getEntry()           { return entry; }
     public void setLevel(int level) { this.level = level; }
     public int getLevel()           { return level; }
-
     public CreatureAI getAI()       { return ai; }
+    
+    /**
+     * Update is called every diff (default: 100 ms) and can be used for a variety of things. For now, it is only used for AI
+     * @param diff Time since update was last called (generally around 100 ms by default)
+     */
     @Override
     public void update(int diff)
     {
         super.update(diff);
+        // Updates the AI
         ai.updateAI(diff);
     }
     
+    // The functions below are currently unused - they were going to be used during battles and allowed more AI power and flexibility
+    /**
+     * Called everytime the creature is healed
+     * @param heal The amount the creature is healed by
+     * @param healer The unit healing the creature
+     * @return The (possibly) modified heal amount
+     */
     @Override
     public int onHealReceived(int heal, Unit healer) 
     {
-        return heal;
+        // Call the AI's onHealReceived, allowing the AI to control what happens if this unit is healed
+        return ai.onHealReceived(heal, healer);
     }
 
+    /**
+     * Called everytime the creature takes damage
+     * @param damage The amount of damage taken
+     * @param attacker The unit attacking the creature
+     * @return The (possibly) modified damage amount
+     */
     @Override
     public int onDamageTaken(int damage, Unit attacker) 
     {
-        return damage;
+        // Call the AI's onDamageTaken, allowing it to control what happens
+        return ai.onDamageTaken(damage, attacker);
     }
 
+    /**
+     * Called everytime the creature deals damage
+     * @param damage The amount of damage dealt
+     * @param target The target to damage
+     * @param spell The spell being cast (possibly)
+     * @param is_auto_attack A flag indicating whether or not this attack is an auto attack
+     * @return The amount of damage being dealt (after any possibly modifiers)
+     */
     @Override
     public int onDamageDeal(int damage, Unit target, Spell spell, boolean is_auto_attack) 
     {
-        return damage;
+        // Call the AI's onDamageDeal, allowing it control
+        return ai.onDamageDeal(damage, target, spell, is_auto_attack);
     }
 
+    /**
+     * Called everytime the creature is hit by a spell
+     * @param spell The spell the creature is hit by
+     * @param caster The unit casting the spell
+     */
     @Override
-    public void onSpellHit(Spell spell, Unit caster) {}
+    public void onSpellHit(Spell spell, Unit caster) 
+    {
+        // Call the AI's onSpellHit, giving it control
+        ai.onSpellHit(spell, caster);
+    }
+    
+    /**
+     * Called everytime the creature casts a spell
+     * @param spell The spell being cast
+     * @param target The target of the spell
+     */
     @Override
-    public void onSpellCast(Spell spell, Unit target) {}
+    public void onSpellCast(Spell spell, Unit target) 
+    {
+        // Give the AI control
+        ai.onSpellCast(spell, target);
+    }
+    
+    /**
+     * Called when the creature dies (health reaches 0)
+     * @param killer The unit that dealt the last bit of damage, killing the creature
+     */
     @Override
-    public void onDeath(Unit killer)  {}
+    public void onDeath(Unit killer)  
+    {
+        // AI has control
+        ai.onDeath(killer);
+    }
 }
--- src/server/game/CreatureAI.java
+++ src/server/game/CreatureAI.java
@@ -4,11 +4,18 @@ import server.game.ai.DoNothingAI;
 import server.game.ai.GenericAI;
 import server.handlers.GameHandler;
 
+/**
+ * This is an abstract AI class. Every unique AI that is created is a subclass of this AI.
+ */
 public abstract class CreatureAI 
 {
-    protected Creature m_creature;
-    protected Map m_instance;
+    protected Creature m_creature; // The creature that this AI belongs to
+    protected Map m_instance; // The map that the creature is in
     
+    /**
+     * Constructs a new CreatureAI for the creature
+     * @param creature The creature for which this AI belongs to
+     */
     public CreatureAI(Creature creature)
     {
         System.out.println("Creating CreatureAI - MapID: " + creature.getMap());
@@ -16,17 +23,29 @@ public abstract class CreatureAI
         this.m_instance = GameHandler.instance().getMap(m_creature.getMap());
     }
     
+    /**
+     * Simple static function to properly load an AI for a creature
+     * @param name The name of the AI to be loaded
+     * @param creature The creature that the AI is loaded for
+     * @return A new instance of the specified AI
+     */
     public static CreatureAI getAIByName(String name, Creature creature)
     {
+        // Just do a simple string comparision chain until we find the proper AI name
         if(name.equalsIgnoreCase("GenericAI"))
             return new GenericAI(creature);
-        if(name.equalsIgnoreCase("DoNothingAI"))
+        else if(name.equalsIgnoreCase("DoNothingAI"))
             return new DoNothingAI(creature);
         
         // By default, return the idle AI
         return new DoNothingAI(creature);
     }
     
+    /**
+     * Helper function to spawn a new creature within this map on a random location
+     * @param creature_entry The entry of the creature to be spawned
+     * @return The spawned creature
+     */
     public Creature summonCreature(int creature_entry)
     {
         float px = m_instance.getRandX();
@@ -34,34 +53,74 @@ public abstract class CreatureAI
         return summonCreature(creature_entry, px, py);
     }
     
+    /**
+     * Helper function to spawn a new creature on the specified coordinates of this map
+     * @param creature_entry The entry of the creature to be spawned
+     * @param tx x-coordinate to spawn the creature on
+     * @param ty y-coordinate to spawn the creature on
+     * @return The spawned creature
+     */
     public Creature summonCreature(int creature_entry, float tx, float ty)
     {
+        // Creatures (and their AI) cannot actually spawn a new creature directly, only maps can so we make our map instance spawn it
         Creature cr = m_instance.spawnCreature(creature_entry, tx, ty);
-        this.onSummon(cr);
+        this.onSummon(cr); // Call the AI's onSummon function (since we just spawned a creature) - allows more scripting power
         
         return cr;
     }
     
+    /**
+     * Finds a specific creature instance within the map using the creature's GUID
+     * @param cguid The GUID of the desired creature
+     * @return The creature that the GUID corresponds to (or NULL if not found)
+     */
     public Creature getCreatureFromMap(int cguid)
     {
         return m_instance.getCreature(cguid);
     }
     
+    /**
+     * Despawns a specific creature from the map using the creature's GUID
+     * @param cguid The GUID of the creature to be despawned
+     */
     public void despawnCreature(int cguid)
     {
         System.err.println("Despawning " + cguid);
         
+        // Similar to summoning, only the map can despawn creatures 
         m_instance.despawnCreature(cguid);
     }
     
-    public void onDamageTaken(Unit attacker, int damage) {}
+    // These functions are called by identical functions from the Creature class, and can be overrided within individual AI scripts for various reasons.
+    public int onHealReceived(int heal, Unit healer)    { return heal;   }
+    public int onDamageTaken(int damage, Unit attacker) { return damage; }
+    public int onDamageDeal(int damage, Unit target, Spell spell, boolean auto) { return damage; }
     public void onDeath(Unit killer) {}
     public void onSummon(Creature summoned) {}
-    public void onSpellHit(Unit caster, Spell spell) {}
-    public void onSpellCast(Unit target, Spell spell) {}
+    public void onSpellHit(Spell spell, Unit caster) {}
+    public void onSpellCast(Spell spell, Unit target) {}
     
-    public void castSpell(Spell spell, Unit target) {}
+    /**
+     * Casts a spell towards a target
+     * @param spell The spell being cast
+     * @param target The target of the spell
+     */
+    public void castSpell(Spell spell, Unit target) 
+    { 
+        // The AI simply calls the unit's cast spell function - this method is just convenience (even if unused)
+        m_creature.castSpell(spell, target); 
+    }
+    
+    /**
+     * Called whenever a player completes a movement
+     * @param pl The player who completed the movement
+     */
     public void onPlayerMoveComplete(Player pl) {}
     
+    /**
+     * The most important function of the AI (and so MUST be overrided), is called by the creature every diff.
+     * This function allows the AI to make use of custom timers and so will be the meat of most AI scripts
+     * @param diff Time (in ms) since this function was last called (default around 100 ms)
+     */
     public abstract void updateAI(int diff);
 }
