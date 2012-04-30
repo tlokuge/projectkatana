fc38b281b48e40483acd54d54122d6faaf5df60e
dragon.thavisha@gmail.com <dragon.thavisha@gmail.com@c9ead0c7-6394-5fdd-6956-1982a9dc6c1c>
2012-04-30 07:52:06 +0000

    * Run UpdateThread in the main thread rather than its own separate one. * Some small cleanup.

--- trunk/src/server/main/ServerMain.java
+++ trunk/src/server/main/ServerMain.java
@@ -1,15 +1,9 @@
 package server.main;
 
 import server.communication.KatanaServer;
-import java.util.Random;
-import server.game.Creature;
-import server.game.Map;
 import server.handlers.SQLHandler;
 import server.handlers.UpdateThread;
 import server.shared.Constants;
-import server.shared.KatanaPacket;
-import server.shared.KatanaSocket;
-import server.shared.Opcode;
 import server.utils.Config;
 import server.utils.SQLCache;
 
@@ -28,76 +22,11 @@ public class ServerMain
                 Integer.parseInt(Config.getConfig(Constants.CONFIG_SERVER_PORT)));
         SQLCache.createCache();
         
-        UpdateThread update = new UpdateThread(
+        UpdateThread updater = new UpdateThread(
                 Integer.parseInt(Config.getConfig(Constants.CONFIG_UPDATE_DIFF)),
                 Integer.parseInt(Config.getConfig(Constants.CONFIG_PING_INTERVAL)),
                 Integer.parseInt(Config.getConfig(Constants.CONFIG_MAX_PINGS)));
-        
-    }
-    
-    // Remove this method, just for testing
-    public static void testStuff()
-    {
-        try
-        {
-            // Very hacky, but required because otherwise packets are sent immediately to the server
-            // Since there are multiple threads running here, the server thread might not be ready 
-            // (since the singleton may not be set up yet), and given the way it's been coded - it crashes.
-            // This is intentional because if we somehow are unable to set up the Server's Singleton thread...
-            //and attempt to access it, we should kill everything instead of letting things like SQL or Ping run..
-            Thread.currentThread().sleep(1000);
-            
-            // Streessss testtt
-            for(int i = 100; i < 200; ++i)
-            {
-                KatanaSocket client = new KatanaSocket("localhost", 7777);
-                Thread.currentThread().sleep(1000);
-
-                KatanaPacket packet = new KatanaPacket(Opcode.C_REGISTER);
-                packet.addData(i + ""); // username
-                packet.addData(i + ""); // password
-                client.sendPacket(packet);
-                Thread.currentThread().sleep(500);
-
-                packet = new KatanaPacket(Opcode.C_ROOM_LIST);
-                packet.addData("90");
-                packet.addData("90");
-                client.sendPacket(packet);
-                Thread.currentThread().sleep(500);
-
-                packet = new KatanaPacket(Opcode.C_ROOM_CREATE);
-                packet.addData("Room " + i);
-                packet.addData((new Random(System.currentTimeMillis()).nextInt(3) + 1) + "");
-                packet.addData("4");
-                packet.addData("2");
-                client.sendPacket(packet);
-                Thread.currentThread().sleep(500);
-                /*
-                packet = new KatanaPacket(Opcode.C_ROOM_JOIN);
-                packet.addData("1");
-                packet.addData("1");
-                client.sendPacket(packet);
-                Thread.currentThread().sleep(10000);
-
-                packet = new KatanaPacket(Opcode.C_CLASS_CHANGE);
-                packet.addData("2");
-                client.sendPacket(packet);
-                Thread.currentThread().sleep(10000);
-
-                packet = new KatanaPacket(Opcode.C_ROOM_LEAVE);
-                packet.addData("2");
-                client.sendPacket(packet);
-                Thread.currentThread().sleep(10000);
-
-                Thread.currentThread().sleep(360000);
-                packet = new KatanaPacket(Opcode.C_LOGOUT);
-                client.sendPacket(packet);*/
-            }
-        }
-        catch(Exception ex)
-        {
-            ex.printStackTrace();
-        }
+        updater.start();
     }
     
     public static void main(String[] args)
@@ -110,6 +39,5 @@ public class ServerMain
         System.out.println(" |_|\\_|_/    \\_\\_/_/    \\_\\_| \\_/_/    \\_\\");
         
         setupServer();
-        //testStuff();
     }
 }
